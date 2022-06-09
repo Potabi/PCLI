9488cd67e9fd695fc590d22655325dc1e7ce5898
Kai Lyons <loralighte@pop-os.localdomain>
2022-06-09 13:40:57 -0600

    New build file

---
+++ build.new.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+# The new build.sh file for proper installation.
+# The goal of this project is unlike the Potabi Build Tool
+# Is to install the actual system. Initial tests of this
+# tool is to follow various manual installation methods online,
+# pick the best ones, and finally modify it to install a fully
+# functional Potabi system.

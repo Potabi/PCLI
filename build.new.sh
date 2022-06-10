d12ffcab5e0ec4051a840825e5a2922f34bc0565
Kai Lyons <loralighte@pop-os.localdomain>
2022-06-09 19:39:07 -0600

    Comment unneeded stuff out

--- build.new.sh
+++ build.new.sh
@@ -53,12 +53,12 @@ chroot ${install} echo "zfs_enable=\"YES\"" >> /etc/rc.conf
 chroot ${install} echo "ifconfig_re0=\"DHCP\"" >> /etc/rc.conf
 chroot ${install} echo "zfs_load=\"YES\"" >> /boot/loader.conf
 chroot ${install} echo "zfs.root.mountfrom=\"zfs:zroot\"" >> /boot/loader.conf
-chroot ${install} ok unload
-chroot ${install} ok load /boot/kernel/kernel
-chroot ${install} ok load /boot/kernel/opensolaris.ko
-chroot ${install} ok load /boot/kernel/zfs.ko
-chroot ${install} ok set currdev="${device}p2"
-chroot ${install} ok set vfs.root.mountfrom="zfs:zroot"
+# chroot ${install} ok unload
+# chroot ${install} ok load /boot/kernel/kernel
+# chroot ${install} ok load /boot/kernel/opensolaris.ko
+# chroot ${install} ok load /boot/kernel/zfs.ko
+# chroot ${install} ok set currdev="${device}p2"
+# chroot ${install} ok set vfs.root.mountfrom="zfs:zroot"
 touch ${install}/etc/fstab
 touch ${install}/etc/resolv.conf
 
